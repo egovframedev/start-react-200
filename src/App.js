88e52537dc4f29949532efff0363aa63baa0587b
egovframedev <whoisy@daum.net>
2021-09-06 13:03:35 +0900

    011. 전개 연산자 사용하기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import Variable from './R010_Variable';
+import SpreadOperator from './R011_SpreadOperator';
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
-      <p>var, let, const 사용하기</p>
-      <Variable />
+      <p>전개 연산자 사용하기</p>
+      <SpreadOperator />
     </div>
   );
 }
