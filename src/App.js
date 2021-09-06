4a1c1f58f18cbbc591c33fd7868056de08a1fa92
egovframedev <whoisy@daum.net>
2021-09-06 13:01:51 +0900

    010. var, let, const 사용하기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import Es6 from './R009_Es6';
+import Variable from './R010_Variable';
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
-      <p>템플릿 문자열 사용하기</p>
-      <Es6 />
+      <p>var, let, const 사용하기</p>
+      <Variable />
     </div>
   );
 }
