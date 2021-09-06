4a38843104f3598d8c4fe781d2e37a219c1aebf8
egovframedev <whoisy@daum.net>
2021-09-06 13:51:50 +0900

    014. forEach() 함수 사용하기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import ArrowFunction from './R013_ArrowFunction';
+import ForEach from './R014_ForEach';
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
-      <p>화살표 함수 사용하기</p>
-      <ArrowFunction />
+      <p>forEach() 함수 사용하기</p>
+      <ForEach />
     </div>
   );
 }
