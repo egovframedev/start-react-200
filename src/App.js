810ddab9f5fabb93ac11453192a883a702e081ee
egovframedev <whoisy@daum.net>
2021-09-08 17:16:30 +0900

    027. Component 사용하기(class형 컴포넌트)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import ForceUpdate from './part2/R026_ForceUpdate';
+import ComponentClass from './part2/R027_ComponentClass';
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
-      <p>State 사용하기</p>
-      <ForceUpdate />
+      <p>Component 사용하기</p>
+      <ComponentClass />
     </div>
   );
 }
