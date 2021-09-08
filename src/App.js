e3568c14fa8c3900663eb997b569a2d84727a130
egovframedev <whoisy@daum.net>
2021-09-08 18:05:46 +0900

    030. 함수형 컴포넌트 사용하기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import ShallowEqual from './part2/R029_ShallowEqual';
+import FunctionComponent from './part2/R030_FunctionComponent';
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>Component 사용하기</p>
-      <ShallowEqual />
+      <FunctionComponent contents="[THIS IS FunctionComponent]" />
     </div>
   );
 }
---
+++ src/part2/R030_FunctionComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+/** 030. 함수형 컴포넌트 사용하기 */
+function R030_FunctionComponent(props) {
+  let { contents } = props;
+
+  return (
+    <h2>{ contents }</h2>
+  )
+}
+
+export default R030_FunctionComponent;
