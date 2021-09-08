abd3e154d882a08bc59758678af5b814fbb2467a
egovframedev <whoisy@daum.net>
2021-09-08 15:20:07 +0900

    025. setState() 함수 사용하기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import ReactState from './part2/R024_ReactState';
+import SetState from './part2/R025_SetState';
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>Props 사용하기</p>
-      <ReactState reactString={"react"} />
+      <SetState />
     </div>
   );
 }
