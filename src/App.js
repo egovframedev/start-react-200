4efd0bdb517f1fa3efb8d9c18b0645c5f95c6903
egovframedev <whoisy@daum.net>
2021-09-10 17:48:19 +0900

    R038. reactstrap Button Group 사용하기

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
-import ReactstrapDropdown from './part2/R037_ReactstrapDropdown';
+import ReactstrapButtonGroup from './part2/R038_ReactstrapButtonGroup';
 import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
-      <p>ReactStrap 사용하기</p>
-      <ReactstrapDropdown />
+      <p>ReactStrap (React + Bootstrap4) 사용하기</p>
+      <ReactstrapButtonGroup />
     </div>
   );
 }
