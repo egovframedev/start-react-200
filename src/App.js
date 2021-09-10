f05e039b1eb388f3b7678ae280e35a19b6949ca0
egovframedev <whoisy@daum.net>
2021-09-10 17:02:11 +0900

    036. reactstrap Breadcrumbs 사용하기

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
-import ReactstrapBadges from './part2/R035_ReactstrapBadges';
+import ReactstrapBreadcrumbs from './part2/R036_ReactstrapBreadcrumbs';
 import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
-      <p>reactstrap Alert 사용하기</p>
-      <ReactstrapBadges />
+      <p>ReactStrap 사용하기</p>
+      <ReactstrapBreadcrumbs />
     </div>
   );
 }
